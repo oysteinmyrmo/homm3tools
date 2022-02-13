6db705f8c7de3767d3b4212300464160d207b9cd
Øystein Myrmo <oystein.myrmo@gmail.com>
2022-02-13 21:14:11 +0100

    Set default values in Hero struct

--- src/h3/hero.h
+++ src/h3/hero.h
@@ -40,31 +40,31 @@ struct Hero
     uint8_t knowledgeInGame() const { return std::min(knowledge, maxPrimarySkillValue); }
 
     // Offsets are relative to name.
-    uint16_t x = -1;                        // -195
-    uint16_t y = -1;                        // -193
-    uint16_t z = -1;                        // -191
-    uint8_t on_map;                         // -189 (1 = on the map, 0 = visiting town or in prison. Position is set when in prison.)
+    uint16_t x = 0xFFFF;                    // -195
+    uint16_t y = 0XFFFF;                    // -193
+    uint16_t z = 0xFFFF;                    // -191
+    uint8_t on_map = 0;                     // -189 (1 = on the map, 0 = visiting town or in prison. Position is set when in prison.)
     uint8_t _unused1[19];
-    uint8_t player;                         // -169
+    uint8_t player = 0xFF;                  // -169
     uint8_t _unused2[14];
     Orientation orientation;                // -154
     uint8_t _unused3[3];
-    uint32_t dest_x = -1;                   // -151
-    uint32_t dest_y = -1;                   // -147
-    uint32_t dest_z = -1;                   // -143
+    uint32_t dest_x = 0xFFFFFFFF;           // -151
+    uint32_t dest_y = 0xFFFFFFFF;           // -147
+    uint32_t dest_z = 0xFFFFFFFF;           // -143
     uint8_t _unused4[4];
-    uint16_t movement_remaining = -1;       // -134
+    uint16_t movement_remaining = 0xFFFF;   // -134
     uint8_t _unused5[10];
-    uint16_t spell_points;                  // -122
+    uint16_t spell_points = 0;              // -122
     uint8_t _unused6[64];
     Creature creatures[creatureCount];      // -56
     uint32_t creature_count[creatureCount]; // -28
     uint8_t name[13];                       // 0
     uint8_t _unused7[56];                   // +13
-    uint8_t attack;                         // +82
-    uint8_t defense;                        // +83
-    uint8_t power;                          // +84
-    uint8_t knowledge;                      // +85
+    uint8_t attack = 0;                     // +82
+    uint8_t defense = 0;                    // +83
+    uint8_t power = 0;                      // +84
+    uint8_t knowledge = 0;                  // +85
     uint8_t _unused8[879];                  // +86
 } __attribute__((__packed__));
 
