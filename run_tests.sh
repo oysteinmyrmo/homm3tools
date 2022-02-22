0303bd4eb3a6338f850ca80e39ab6af6eb2b9f39
Øystein Myrmo <oystein.myrmo@gmail.com>
2022-02-22 21:08:58 +0100

    Add script to run unit tests

---
+++ run_tests.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -vx
+set -e
+
+mkdir -p build
+cd build
+
+exit_code=0
+
+if [[ "$OS" == "Windows_NT" ]]; then
+    cmake ..
+    cmake --build . --config Release
+    ctest -C Release --verbose --output-on-failure .
+    exit_code=$?
+else
+    cmake -DCMAKE_BUILD_TYPE=Release ..
+    cmake --build .
+    ctest --verbose --output-on-failure .
+    exit_code=$?
+fi
+
+exit ${exit_code}
